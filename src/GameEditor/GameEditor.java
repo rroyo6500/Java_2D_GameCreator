8935f22dc80112b47cc37510a84ee88032d09292
Rroyo <rroyo@proton.me>
2024-12-16 22:58:56 +0100

    .

--- src/GameEditor/GameEditor.java
+++ src/GameEditor/GameEditor.java
@@ -28,7 +28,7 @@ public class GameEditor {
     public static int Width = 1000, Height = 1000;
     public static Color BgColor = Color.black;
 
-    Player pj = new Player(475, 0, 50, 50);
+    Player player = new Player(475, 0, 50, 50);
     Platform platform = new Platform(495, 900, 10, 100);
     Enemy enemy = new Enemy(10, 950, 50, 50);
     Colectable coin = new Colectable(750, 970, 10, 10);
@@ -53,27 +53,27 @@ public class GameEditor {
 
                 if ((K.getLeftKey() || K.getCustomKey("A")) && (K.getRightKey() || K.getCustomKey("D")) && Suelo){
                     Suelo = false;
-                    pj.setVelocity(pj.getVelocityX(), -25);
+                    player.setVelocity(player.getVelocityX(), -10);
                 } else if ((K.getUpKey() || K.getCustomKey("W")) && Suelo) {
                     Suelo = false;
-                    pj.setVelocity(pj.getVelocityX(), -25);
+                    player.setVelocity(player.getVelocityX(), -25);
                 } else {
-                    if (K.getLeftKey() || K.getCustomKey("A")) pj.setVelocity(-4, pj.getVelocityY());
-                    if (K.getRightKey() || K.getCustomKey("D")) pj.setVelocity(4, pj.getVelocityY());
+                    if (K.getLeftKey() || K.getCustomKey("A")) player.setVelocity(-4, player.getVelocityY());
+                    if (K.getRightKey() || K.getCustomKey("D")) player.setVelocity(4, player.getVelocityY());
                 }
 
-                pj.setVelocity(0, pj.getVelocityY() + 2);
-                if (pj.getFaceDown("y") > Height){
+                player.setVelocity(0, player.getVelocityY() + 2);
+                if (player.getFaceDown("y") > Height){
                     Suelo = true;
-                    pj.setVelocity(0, 0);
-                    pj.setY(Width - pj.getHeight());
+                    player.setVelocity(0, 0);
+                    player.setY(Width - player.getHeight());
                 }
 
-                if (C.collide(pj, enemy, "up")){
+                if (C.collide(player, enemy, "up")){
                     Suelo = true;
                     D.delete(enemy);
-                } else if (O.overlap(pj, enemy)) {
-                    D.delete(pj);
+                } else if (O.overlap(player, enemy)) {
+                    D.delete(player);
                 }
 
                 if (enemy_Left) enemy.setVelocity(4, 0);
@@ -82,15 +82,16 @@ public class GameEditor {
                 if (C.collide(enemy, platform, "left")) enemy_Left = !enemy_Left;
                 else if (enemy.getFaceLeft("x") <= 0) enemy_Left = !enemy_Left;
 
-                if (O.overlap(coin, pj)){
+                if (O.overlap(coin, player)){
                     D.delete(coin);
                 }
 
-                C.collide(pj, platform, "all");
-                if (C.collide(pj, platform, "up")){
+                C.collide(player, platform, "all");
+                if (C.collide(player, platform, "up")){
                     Suelo = true;
                 }
 
+                D.draw(g, player);
                 D.draw(g, coin);
                 D.draw(g, platform);
                 D.draw(g, enemy);
